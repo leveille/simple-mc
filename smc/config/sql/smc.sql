c20c63f1050fb6ab79859dbe6bad28fd72109e9b
Jason Leveille <leveillej@gmail.com>
2009-01-29 16:19:21 -0500

    Updated SQL files

--- smc/config/sql/smc.sql
+++ smc/config/sql/smc.sql
@@ -18,14 +18,14 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Table structure for table `smc_blocks`
 --
-
+DROP TABLE IF EXISTS `smc_blocks`;
 CREATE TABLE `smc_blocks` (
   `id` smallint(5) unsigned NOT NULL auto_increment,
   `block` mediumtext collate utf8_unicode_ci NOT NULL,
   `description` varchar(255) collate utf8_unicode_ci NOT NULL,
   `locked` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=31 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `smc_blocks`
@@ -37,7 +37,7 @@ CREATE TABLE `smc_blocks` (
 --
 -- Table structure for table `smc_roles`
 --
-
+DROP TABLE IF EXISTS `smc_roles`;
 CREATE TABLE `smc_roles` (
   `id` tinyint(3) unsigned NOT NULL auto_increment,
   `role` varchar(30) collate utf8_unicode_ci NOT NULL,
@@ -57,7 +57,7 @@ INSERT INTO `smc_roles` (`id`, `role`) VALUES
 --
 -- Table structure for table `smc_users`
 --
-
+DROP TABLE IF EXISTS `smc_users`;
 CREATE TABLE `smc_users` (
   `id` tinyint(3) unsigned NOT NULL auto_increment,
   `username` varchar(15) collate utf8_unicode_ci NOT NULL,
