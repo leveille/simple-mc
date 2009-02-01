c157d2160c1b96dccd146e5d15ef5a449d4945a5
Jason Leveille <leveillej@gmail.com>
2009-02-01 14:23:44 -0500

    Removed old demo, added new

---
+++ smc/tmp/demo2/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="title" content="">
+        <meta name="description" content="">
+        <meta name="keywords" content="">         
+        <meta http-equiv="Content-Language" content="en-us">
+        <meta name="author" content="Blue Atlas Interactive">
+        <meta name="copyright" content="http://blueatlas.com">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <meta name="ICBM" content="lat, long">
+        <meta name="geo.position" content="lat, long">
+        <meta name="geo.placename" content="city, state">
+        <meta name="geo.region" content="US-MD">
+        <meta name="geo.country" content="US">
+
+        <title>{Site Name}</title>
+        
+        <link rel="stylesheet" type="text/css" href="/assets/css/base.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="/assets/css/print.css" media="print">
+    </head>
+    <body>
+        <div id="doc" class="yui-t5">
+            
+            <div id="hd">
+                <div id="meta">
+                    link | link | link
+                </div>
+                
+                <div id="logo">
+                    <h1>h1: Client Name</h1>
+                    <p>p: Motto/subheading/mantra/etc</p>
+                </div>
+                
+                <div id="nav">
+                    <ul>
+                        <li>
+                            <a href="#" title="">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#" title="">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#" title="">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#" title="">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#" title="">Link 1</a>
+                        </li>
+                    </ul>
+                </div><!-- closing nav -->
+            </div><!-- closing hd -->
+            
+            <div id="bd">
+                <div id="yui-main">
+                    <div class="yui-b">
+                        <div class="pad">
+                            <h2>h2: Main Content</h2>
+                            <p>
+                                <img src="/assets/images/bg/100.jpg" alt="A test jpg" title="Test Jpg">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus sollicitudin risus. In non velit et tellus tincidunt sagittis. Vestibulum interdum tincidunt tortor. Duis iaculis feugiat dui. Duis fermentum. Nam ullamcorper mi nec nulla. Curabitur congue fringilla lacus. Nam id neque tincidunt sem porta luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam feugiat ornare lacus. 
+                            </p>
+                            <p>
+                                <img src="/assets/images/bg/pic.gif" alt="A test gif" title="Test Gif" class="right">Maecenas fringilla porttitor lacus. Pellentesque interdum. Aenean viverra, lacus eu tristique rhoncus, nulla nunc blandit erat, ut pellentesque neque odio consequat urna. Mauris porta lacus in nulla. Aenean odio nibh, pharetra in, malesuada sed, iaculis ut, sem. Donec posuere. Vivamus cursus tellus ac tortor. Duis sed nunc. Vestibulum bibendum. Quisque metus nibh, iaculis ac, lacinia vel, rhoncus et, justo. Curabitur non massa ac dolor feugiat malesuada. Aliquam suscipit. Aliquam consectetuer, erat nec luctus ornare, leo nisi faucibus tortor, vitae vestibulum ligula est eget arcu. Integer ultrices gravida nisi. Vestibulum elementum diam in eros. Mauris tellus massa, feugiat eu, consequat sit amet, tincidunt et, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas dolor. Sed eu ipsum. Mauris sit amet diam. 
+                            </p>
+                            <p>
+                                <img src="/assets/images/bg/test.jpg" alt="A test jpg" title="Raul Ortiz">Nullam nunc. Sed sed turpis id dolor blandit vehicula. Nulla luctus. Praesent euismod nulla in orci. Phasellus mattis mi ut urna. Nam aliquet iaculis tortor. Donec bibendum, nulla id sagittis malesuada, enim felis auctor tellus, in eleifend diam dui quis purus. Sed tellus quam, commodo sit amet, vestibulum sed, tempor in, elit. Ut ante ante, mattis quis, fringilla in, rutrum eget, mi. Fusce consequat, tortor eu egestas porttitor, urna nisl scelerisque odio, quis auctor mi massa sagittis orci. Curabitur mauris. Mauris lacinia. Duis cursus, erat nec interdum vulputate, dui erat sollicitudin turpis, vitae pulvinar velit tellus a tortor. 
+                            </p>
+                            <p>
+                                Quisque placerat. Donec et nunc et mauris convallis bibendum. Donec eu diam sodales felis sagittis volutpat. Suspendisse fermentum condimentum sem. Nulla facilisi. Integer nec dui. Pellentesque sodales justo sit amet erat. Nulla feugiat, libero in consectetuer luctus, velit enim placerat arcu, tempor auctor nisl nunc non est. Praesent non dui. Sed lacus lectus, dapibus eu, dapibus ac, euismod nec, metus.
+                            </p>
+                        </div>
+                    </div><!-- closing yui-b: main --->
+                </div><!-- closing yui-main -->
+                
+                <div class="yui-b">
+                    <div class="pad">
+                        <h3>h3: Sidebar</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus sollicitudin risus. In non velit et tellus tincidunt sagittis. Vestibulum interdum tincidunt tortor. Duis iaculis feugiat dui. Duis fermentum. Nam ullamcorper mi nec nulla. Curabitur congue fringilla lacus. Nam id neque tincidunt sem porta luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam feugiat ornare lacus. 
+                        </p>
+                    </div>
+                </div><!-- closing yui-b: side --->
+            </div><!-- closing bd -->
+            
+            <div id="ft">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+            </div><!-- closing ft -->
+        </div><!-- closing doc -->
+        
+        <!--http://developer.yahoo.com/performance/rules.html#js_bottom-->
+        <script src="/assets/javascripts/jquery/jquery.min.js"></script>
+        <script src="/assets/javascripts/application.js"></script>
+
+        <script type="text/javascript">
+            //A quick check to see if we are on a page that requires flash replacement
+            //id-to-replace should be replaced with the id of the div/element to replace
+            if ($('#id-to-replace').length > 0) {
+                //jquery specific code here.  If not including the jquery library in a project this
+                //will need modification/removal.  Also, thought will need to be put into the instance
+                //when there are multiple swf files that need to be served throughout the site
+                $.getScript("/javascripts/swfobject/swfobject.js", function(){
+                    var flashvars = {};
+                    var params = {
+                        allowFullScreen: 'true',
+                        wmode: 'opaque'
+                    };
+                    var attributes = {};
+                    swfobject.embedSWF('/local/path/to/swf', "id-to-replace", "width", "height", "9.0.0", null, flashvars, params, attributes);
+                });
+            }
+        </script>
+    </body>
+</html>
